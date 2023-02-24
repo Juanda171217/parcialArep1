405578c210c60bdd0d791b5567a24c020908987d
Juan David <juandamarcar@hotmail.com>
2023-02-24 15:50:24 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Parcial
+
+## Juan David Martinez
+
+# Uso
+
+Clon el repositorio con 
+
+´´´
+git clone https://github.com/Juanda171217/parcialArep1
+
+´´´
+
+Para ejecutar:
+
+´´´
+mvn package
+
+´´´
+
+y luego 
+
+´´´
+java -cp "target/classes" edu.escuelaing.HttpServer
+
+´´´
---
+++ src/main/java/edu/escuelaing/HttpServer.java
@@ -0,0 +1,74 @@
+package edu.escuelaing;
+
+import java.net.*;
+
+import java.io.*;
+
+public class HttpServer {
+
+    private static HttpServer instance = new HttpServer();
+
+    public static HttpServer getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer httpserver = HttpServer.getInstance();
+        try {
+            httpserver.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void start() throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35001);
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        boolean exit = true;
+        while (exit) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            outputLine = "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "\r\n"
+                    + "<!DOCTYPE html>\n"
+                    + "<html>\n"
+                    + "<head>\n"
+                    + "<meta charset=\"UTF-8\">\n"
+                    + "<title>Parcial</title>\n"
+                    + "</head>\n"
+                    + "<body>\n"
+                    + "<h1>Parcial Arep</h1>\n"
+                    + "</body>\n"
+                    + "</html>\n";
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+}
